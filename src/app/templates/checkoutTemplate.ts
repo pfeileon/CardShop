a6edde0948857ef880ce5131b3559c24dad8277e
pfeileon <leonhard.pfeiffer@gmail.com>
2017-02-13 15:26:35 +0100

    checkoutTemplate with Form TODO: layout

--- src/app/templates/checkoutTemplate.ts
+++ src/app/templates/checkoutTemplate.ts
@@ -6,13 +6,46 @@ export const checkoutTemplate = (rService: RenderService) => {
 
     <h1 class="well">Checkout</h1>
     
-    <section id="personal-data" class="row display-in-line">
-        
-    </section>
-    <section id="checkout-btns" class="row">
+    <form id="personal-data">
+            <div class="form-group">
+                <label for="first-name">First Name</label>
+                <input id="first-name" type="text" class="from-control" name="first-name" placeholder="First name" />
+            </div>
+            <div class="form-group">
+                <label for="last-name">Last Name</label>
+                <input id="last-name" type="text" class="from-control" name="last-name" placeholder="Last name" />
+            </div>
+            <div class="form-group">
+                <label for="address">Address</label>
+                <input id="address" type="text" class="from-control" name="address" placeholder="Address" />
+            </div>
+            <div class="form-group">
+                <label>Town</label>
+                <input id="town" type="text" class="from-control" name="town" placeholder="Town" />
+            </div>
+            <div class="form-group">
+                <label for="country">Country</label>
+                <input id="country" type="text" class="from-control" name="country" placeholder="Country" />
+            </div>
+            <div class="form-group">
+                <label for="zip-code">Zip-Code</label>
+                <input id="zip-code" type="text" class="from-control" name="zip-code" placeholder="Zip-Code" />
+            </div>
+            <div class="form-group">
+                <label for="telephone">Telephone</label>
+                <input id="telephone" type="text" class="from-control" name="telephone" placeholder="Telephone" />
+            </div>
+            <div class="form-group">
+                <label for="checkout-email">Email</label>
+                <input id="checkout-email" type="email" class="from-control" name="email" placeholder="Email" />
+            </div>
+            <div class="form-group">
+                <label for="confirm-email">Confirm Email</label>
+                <input id="confirm-email" type="email" class="from-control" name="email" placeholder="Confirm email" />
+            </div>
         <button id="cancel-btn" type="button" class="btn btn-danger">Cancel</button>
-        <button id="confirm-btn" type="button" class="btn btn-success">Confirm</button>
-    </section>
+        <button id="confirm-btn" type="submit" class="btn btn-success">Confirm</button>
+    </form>
 
     </article>`
 }
