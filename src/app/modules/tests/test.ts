7c6318cd5b407fd86aca8e122b557d6b1521c06a
Leonhard Pfeifer <leonhard.pfeifer@ITCCN.LOC>
2017-04-04 10:02:57 +0200

    testing

--- src/app/modules/tests/test.ts
+++ src/app/modules/tests/test.ts
@@ -4,3 +4,16 @@ export function testFetchAllCards(fResource) {
             console.log(JSON.stringify(cards));
         });
 }
+
+export function testSqlApi(fService) {
+    let init = {
+        headers: {
+            "sa": "123user!"
+        },
+        method: "GET"
+    }
+    fService.query("http://localhost:56538/api/CardShopAPI", init)
+        .then(cards => {
+            console.log(cards);
+        });
+}
